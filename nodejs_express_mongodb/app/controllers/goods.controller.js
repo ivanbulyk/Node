4e4800b2e88ff9410141b86aa026dae3c62de98e
Ivan Bulyk <stackyogi@gmail.com>
2020-10-24 01:43:19 +0300

    Create the Controller

---
+++ nodejs_express_mongodb/app/controllers/goods.controller.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+const Goods = db.goods;
+
+// Create and Save a new Good
+exports.create = (req, res) => {
+  
+};
+
+// Retrieve all Goods from the database.
+exports.findAll = (req, res) => {
+  
+};
+
+// Find a single Good with an id
+exports.findOne = (req, res) => {
+  
+};
+
+// Update a Good by the id in the request
+exports.update = (req, res) => {
+  
+};
+
+// Delete a Good with the specified id in the request
+exports.delete = (req, res) => {
+  
+};
+
+// Delete all Goods from the database.
+exports.deleteAll = (req, res) => {
+  
+};
+
+// Find all in stock Good
+exports.findAllinStock = (req, res) => {
+  
+};
